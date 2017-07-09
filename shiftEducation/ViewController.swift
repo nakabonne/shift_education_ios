7062c96ad9f1444ebf97b5e92774ff5954740d26
nakaoC <r.nakao.cubic@gmail.com>
2017-07-09 15:30:51 +0900

    Initial Commit

---
+++ shiftEducation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shiftEducation
+//
+//  Created by 中尾涼 on 2017/07/09.
+//  Copyright © 2017年 中尾涼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
